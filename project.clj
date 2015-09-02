ee737b56d65004dcbd9906f436ec04ebfd9811d6
ESC <youhavethewrong@gmail.com>
2015-09-01 21:18:26 -0500

    initial structure

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ml "0.1.0-SNAPSHOT"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.37.0"]
+                 [clatrix "0.5.0"]
+                 [incanter/incanter-core "1.9.0"]])
